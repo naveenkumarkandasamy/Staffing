71a007309f562e2148d8d78bb12c640cb0afcf5a
payal <payal.mantri@accoliteindia.com>
2019-12-26 19:53:26 +0530

    Add: API for login url

--- src/main/java/com/envision/Staffing/auth/CustomAuthenticationProvider.java
+++ src/main/java/com/envision/Staffing/auth/CustomAuthenticationProvider.java
@@ -11,24 +11,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
-    @Override
-    public Authentication authenticate(Authentication auth) 
-      throws AuthenticationException {
-        String username = auth.getName();
-        String password = auth.getCredentials()
-            .toString();
- 
-        if ("osatuser".equalsIgnoreCase(username) && "osatuser123".equals(password)) {
-            return new UsernamePasswordAuthenticationToken
-              (username, password, Collections.emptyList());
-        } else {
-            throw new
-              BadCredentialsException("External system authentication failed");
-        }
-    }
- 
-    @Override
-    public boolean supports(Class<?> auth) {
-        return auth.equals(UsernamePasswordAuthenticationToken.class);
-    }
+	@Override
+	public Authentication authenticate(Authentication auth) throws AuthenticationException {
+		String username = auth.getName();
+		String password = auth.getCredentials().toString();
+
+		if ("osatuser".equalsIgnoreCase(username) && "osatuser123".equals(password)) {
+			return new UsernamePasswordAuthenticationToken(username, password, Collections.emptyList());
+		} else {
+			throw new BadCredentialsException("External system authentication failed");
+		}
+	}
+
+	@Override
+	public boolean supports(Class<?> auth) {
+		return auth.equals(UsernamePasswordAuthenticationToken.class);
+	}
 }
--- src/main/java/com/envision/Staffing/auth/MultipleAuthProvidersSecurityConfig.java
+++ src/main/java/com/envision/Staffing/auth/MultipleAuthProvidersSecurityConfig.java
@@ -1,44 +1,33 @@
 package com.envision.Staffing.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class MultipleAuthProvidersSecurityConfig 
-  extends WebSecurityConfigurerAdapter {
-    @Autowired
-    CustomAuthenticationProvider customAuthProvider;
- 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) 
-      throws Exception {
- 
-        auth.authenticationProvider(customAuthProvider);
-        auth.inMemoryAuthentication()
-            .withUser("osatadmin")
-            .password("{noop}osatadmin123")
-            .roles("ADMIN");
-    }
- 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
-            .and()
-            .authorizeRequests()
-            .antMatchers("/api/**")
-            .authenticated();
-        http.csrf().disable();    
-    }
-     
-      
+public class MultipleAuthProvidersSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	CustomAuthenticationProvider customAuthProvider;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.authenticationProvider(customAuthProvider);
+		auth.inMemoryAuthentication().withUser("osatadmin").password("{noop}osatadmin123").roles("ADMIN");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest()
+				.authenticated().and().httpBasic();
+	}
+
 //    @Bean
 //    public PasswordEncoder encoder() {
 //        return new BCryptPasswordEncoder();

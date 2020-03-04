be74c9ce75ba69caad2070886657ba0e8ce982d2
Aparna <aparna@accoliteindia.com>
2020-03-04 20:29:22 +0530

    Removed hard-coded jobId

--- src/main/java/com/envision/Staffing/services/JobListService.java
+++ src/main/java/com/envision/Staffing/services/JobListService.java
@@ -31,7 +31,7 @@ public class JobListService {
 	public boolean deleteJobById(String jobId) {
 		boolean flag;
 		try{
-			JobDetails jobDetails = jobDetailsService.getJobDetailsById("5af50e5f-2ecb-4975-8686-2cfea4891fb9");
+			JobDetails jobDetails = jobDetailsService.getJobDetailsById(jobId);
 			
 			jobDetailsRepository.deleteJobDetailById(jobId); // deletes job and clinicians
 			if(jobDetails.getInputFormat().contentEquals("FTP_URL")) {

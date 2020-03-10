5a5549c6929f2e04e26e7d9fdca03637586c7937
Aparna <aparna@accoliteindia.com>
2020-03-10 10:20:00 +0530

    ConnectionPool Configuration

---
+++ src/test/java/com/envision/Staffing/ConnectionPoolTest.java
@@ -0,0 +1,25 @@
+package com.envision.Staffing;
+
+
+import javax.sql.DataSource;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConnectionPoolTest {
+    
+    @Autowired
+    private DataSource dataSource;
+    
+	@Test
+    public void givenTomcatConnectionPoolInstance_whenCheckedPoolClassName_thenCorrect() {
+    	Assert.assertEquals(dataSource.getClass().getName(),"org.apache.tomcat.jdbc.pool.DataSource");
+    }
+}

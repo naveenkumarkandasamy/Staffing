897ddf8564ae3e804851f5c6b89f1ec2188dcebf
Aparna <aparna@accoliteindia.com>
2020-04-02 20:23:32 +0530

    JUnit Test cases

--- src/test/java/com/envision/Staffing/AllTestsSuite.java
+++ src/test/java/com/envision/Staffing/AllTestsSuite.java
@@ -1,13 +1,13 @@
-package com.envision.Staffing;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-import com.envision.Staffing.services.ShiftCalculatorTest;
-
-@RunWith(Suite.class)
-@SuiteClasses({ ShiftCalculatorTest.class })
-public class AllTestsSuite {
-
-}
+//package com.envision.Staffing;
+//
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Suite;
+//import org.junit.runners.Suite.SuiteClasses;
+//
+//import com.envision.Staffing.services.ShiftCalculatorTest;
+//
+//@RunWith(Suite.class)
+//@SuiteClasses({ ShiftCalculatorTest.class })
+//public class AllTestsSuite {
+//
+//}

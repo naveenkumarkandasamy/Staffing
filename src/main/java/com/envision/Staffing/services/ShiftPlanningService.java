06d93b5cdf820295231df8213884089fcf483759
GowsalyaSG <gowsalya.sg@accoliteindia.com>
2020-04-14 13:05:40 +0530

    Made proper format for input excel file

--- src/main/java/com/envision/Staffing/services/ShiftPlanningService.java
+++ src/main/java/com/envision/Staffing/services/ShiftPlanningService.java
@@ -64,7 +64,7 @@ public class ShiftPlanningService {
 				workload[i].setName(days[i]);
 				Double[] personPerHour = new Double[24];
 				for (int j = 0; j <= 23; j++) {
-					personPerHour[j] = Double.valueOf(myExcelSheet.getRow(i).getCell(j).getNumericCellValue());
+					personPerHour[j] = Double.valueOf(myExcelSheet.getRow(i+1).getCell(j+1).getNumericCellValue());
 				}
 				workload[i].setExpectedPatientsPerHour(personPerHour);
 			}
@@ -209,7 +209,9 @@ public class ShiftPlanningService {
 				for (Double patientsPerHour : eachDay.getExpectedPatientsPerHour()) {
 					work.getFixedworkloadArray()[k] = patientsPerHour;
 					work.getWorkloadArray()[k] = work.getFixedworkloadArray()[k];
+					System.out.print(k+","+work.getFixedworkloadArray()[k]+" ");
 					k++;
+					
 				}
 			}
 
